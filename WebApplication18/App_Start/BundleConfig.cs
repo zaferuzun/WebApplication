4c57090ee89d79e6deaa28c582f5ef231c08fb98
uznzafer <uznzafer@hotmail.com>
2020-06-01 18:33:47 +0300

    awd

--- WebApplication18/App_Start/BundleConfig.cs
+++ WebApplication18/App_Start/BundleConfig.cs
@@ -4,7 +4,7 @@ using System.Web.Optimization;
 namespace WebApplication18
 {
     public class BundleConfig
-    {//zafer senin ben gelmişini geçmişinisd
+    {//github
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
